84ad071679a6f4cf3879784234952add8db5fe8d
Honghyeonji <66579773+Honghyeonji@users.noreply.github.com>
2020-11-28 02:03:25 +0900

    update

--- main.py
+++ main.py
@@ -5,10 +5,8 @@ from PyQt5.QtWidgets import QTextEdit, QLineEdit, QToolButton, QLabel
 from PyQt5.QtGui import *
 from PyQt5 import uic
 import sys
-# import socket
-# import server
 import yut
-from NameInput import NameInput
+import Nameinput
 
 port = 2020
 
@@ -19,20 +17,23 @@ class WindowClass(QMainWindow, formGame) :
         super().__init__()
         self.setupUi(self)
 
-        # self.s = server.ServerSocket(self)
-        # self.ip = socket.gethostbyname(socket.gethostname())
- 
-        # 닉네임 입력창
-        inputchang = NameInput()
+        # 플레이어1,2 객체 생성
+        self.p1 = yut.Player()
+        self.p2 = yut.Player()
+
+        # p1, p2 닉네임 입력창
+        inputchang = Nameinput.NameInput()
         r = inputchang.showModel()
         if r:
-            self.p2Name = inputchang.inputName.text()
-            self.player2Name.setText(self.p2Name)
+            self.p1.name = inputchang.inputName.text()
+            self.player1Name.setText(self.p1.name)
+            self.p2.name = inputchang.inputName_2.text()
+            self.player2Name.setText(self.p2.name)
 
         # 윷놀이판 배경
         self.background.setStyleSheet('image:url(backgroundEx.png)')
-        # 플레이어 말 이미지 설정
 
+        # 플레이어 말 이미지 설정
         self.player1img = {1: 'image:url(player1_1.png)', 2: 'image:url(player1_2.png)', 3: 'image:url(player1_3.png)', 4: 'image:url(player1_4.png)',
                            12: 'image:url(player1_12.png)', 13: 'image:url(player1_13.png)', 14: 'image:url(player1_14.png)',
                            23: 'image:url(player1_23.png)', 24: 'image:url(player1_24.png)',
@@ -50,49 +51,81 @@ class WindowClass(QMainWindow, formGame) :
         self.emptyCan = "background-color: rgba(255, 255, 255, 0);"    # 말이 놓여져있지 않은 투명한 can 적용을 위한 배경값 설정
 
         for i in range(4):
-            self.playerwait[0][i].setStyleSheet(self.player1img[i+1])
+            self.playerWait[0][i].setStyleSheet(self.player1img[i+1])
         for i in range(4):
             self.playerWait[1][i].setStyleSheet(self.player2img[i+1])
 
         # 윷놀이 판 칸 설정
-        self.cans = [[self.can0_0, self.can0_1, self.can0_2, self.can0_3, self.can0_4],  # [0:3] 모서리 줄
-                      [self.can1_0, self.can1_1, self.can1_2, self.can1_3, self.can1_4],
-                      [self.can2_0, self.can2_1, self.can2_2, self.can2_3, self.can2_4],
-                      [self.can3_0, self.can3_1, self.can3_2, self.can3_3, self.can3_4],
-                      [self.can4_0, self.can4_1],   # [4:7] 가운데 줄
-                      [self.can5_0, self.can5_1],
-                      [self.can6_0, self.can6_1],
-                      [self.can7_0, self.can7_1]]
+        # self.cans = [[self.can0_0, self.can0_1, self.can0_2, self.can0_3, self.can0_4],  # [0:3] 모서리 줄
+        #               [self.can1_0, self.can1_1, self.can1_2, self.can1_3, self.can1_4],
+        #               [self.can2_0, self.can2_1, self.can2_2, self.can2_3, self.can2_4],
+        #               [self.can3_0, self.can3_1, self.can3_2, self.can3_3, self.can3_4],
+        #               [self.can4_0, self.can4_1],   # [4:7] 가운데 줄
+        #               [self.can5_0, self.can5_1],
+        #               [self.can6_0, self.can6_1],
+        #               [self.can7_0, self.can7_1]]
 
+        self.cans = { '00':self.can0_0, '01':self.can0_1, '02':self.can0_2, '03':self.can0_3,
+                      '10':self.can1_0, '11':self.can1_1, '12':self.can1_2, '13':self.can1_3,
+                      '21':self.can2_0, '22':self.can2_1, '33':self.can2_2, '34':self.can2_3,
+                      '31':self.can3_0, '32':self.can3_1, '43':self.can3_2, '44':self.can3_3,
+                      '41':self.can4_0, '42':self.can4_1,
+                      '51':self.can5_0, '52':self.can5_1,
+                      '61':self.can6_0, '62':self.can6_1,
+                      '71':self.can7_0, '72':self.can7_1, }
         self.canMain.setStyleSheet(self.emptyCan)
-        for i in range(4):
-            for j in range(5):
-                self.cans[i][j].setStyleSheet(self.emptyCan)
-            for j in range(2):
-                self.cans[i+4][j].setStyleSheet(self.emptyCan)
+        for key, vaule in self.cans.items():
+            self.cans[key].setStyleSheet(self.emptyCan)
+
+        # for i in range(4):
+        #     for j in range(5):
+        #         self.cans[i][j].setStyleSheet(self.emptyCan)
+        #     for j in range(2):
+        #         self.cans[i+4][j].setStyleSheet(self.emptyCan)
 
         # player1 화면 기준 player2Turn일 경우 버튼비활성화
-        if(self.plyaerTurn.Text() == str(self.p2Name) + " Turn"):
-            self.moveButton.setEnable(True)
-            self.randomYut.setEnable(True)
-        else:
-            self.moveButton.setDisable(True)
-            self.randomYut.setDisable(True)
+        # 이건 소켓서버클라이언트 성공하면 씁니다.
+        # if(self.plyaerTurn.Text() == str(self.p2Name) + " Turn"):
+        #     self.moveButton.setEnable(True)
+        #     self.randomYut.setEnable(True)
+        # else:
+        #     self.moveButton.setDisable(True)
+        #     self.randomYut.setDisable(True)
 
         self.moveButton.clicked.connect(self.MoveButtonClicked)
         self.randomYut.clicked.connect(self.RandomYutButtonClicked)
 
+        self.trun = 1
+        self.playerTurn.setText("현재 차례: " + str(self.turn))
 
             # 윷 던지고 말 이동하기
     def MoveButtonClicked(self):
+        # 가정으로 만든 코드이고 오류 유발해서 주석처리 해놨습니다.
+        # if(self.yutResult.text() != "윷 결과: "):
+        #     if(self.turn == 1):
+        #         i, j, road = p1.(윷이동하는 함수)(self.pNumChoose.text(), self.resultyut)
+        #         self.cans[i].setStyleSheet(self.emptyCan)
+        #         if(int(j) != -1): self.cans[j].setStyleSheet(road)
+        #         # 이건 그냥 제가 가정해서 만들어본겁니다. 만약 다 dic형식으로 한다면 i는 말이 떠난 칸의 위치, j는 말이 있는 위치, road는 말의 이미지경로
+        #         # 이 함수 안에서 말의 번호에 따라 다르게 인자를 넣고 다르게 리턴값을 받는다는 가정입니다!
+        #         # turn을 바꾸는 판정도 정해야합니다~!
+        #     if(self.turn == 2):
+        #
+        # self.yutResult.setText("윷 결과: ")
         pass
 
 
 
     def RandomYutButtonClicked(self):
-        #윷 던지는 함수
-        yr = yut.throw()
-        self.yutResult.setText(str(yr))
+        # 윷 던지는 함수
+        # 오류 유발로 주석처리 해놨습니다.
+        # if(self.turn == 1):
+        #     self.p1.throw()
+        #     self.yutResult.setText("윷 결과: " + self.p1.resultyut)
+        # elif(self.turn == 2):
+        #     self.p1.throw()
+        #     self.yutResult.setText("윷 결과: " + self.p2.resultyut)
+        pass
 
 
 
---
+++ yut.py
@@ -0,0 +1,167 @@
+import random
+# 난수 발생을 위해 random 라이브러리 사용
+'''
+플레이어 이름, 플레이어 결과, 플레이어 이동, 
+찬스는 여기서 생성, 말을 잡거나 모, 윷이 나왔을 때 기회 1번 추가
+말 이동 시 딕셔너리 형태로 하는 것을 가정 {yutArrayNum : player 이미지}
+말이 이동했을 때 이동하는 인덱스
+'''
+
+
+class Player:
+    def __init__(self):
+
+        # 이동 함수에서만 쓰일 pYut = 0  # 사용자의 도,개,걸,윷,모,빽도,낙의 이동해야할 칸수를 저장합니다.
+        # -> 이동함수 인자로 이동시킬 플레이어말넘버와 윷 결과를 넣고
+        # -> 아래의 self.yuts 딕으로 이동시킬 칸을 찾아 pYut에 넣고 그 값에 따라 이동하게
+        self.yutrandoms = []  # 확률 구성 리스트
+        self.yuts = {"빽도":-1, "낙":0, "도":1, "개":2, "걸":3, "윷":4, "모":5}
+        self.name = '' # 플레이어 닉네임
+        self.resultyut = ''
+        # 플레이어
+        self.player1_1 = [0][0]
+        self.player1_2 = [0][0]
+        self.player1_3 = [0][0]
+        self.player1_4 = [0][0]
+
+        self.player2_1 = [0][0]
+        self.player2_2 = [0][0]
+        self.player2_3 = [0][0]
+        self.player2_4 = [0][0]
+
+        #윷놀이판 배열
+        # self.yutArray = [[0][0], [0][1], [0][2], [0][3], [0][4],
+        #                  [1][0], [1][1], [1][2], [1][3], [1][4],
+        #                  [2][0], [2][1], [2][2], [2][3], [2][4],
+        #                  [3][0], [3][1], [3][2], [3][3], [3][4],  # 모서리쪽 칸
+        #                  [4][0], [4][1],
+        #                  [5][0], [5][1],
+        #                  [6][0], [6][1],
+        #                  [7][0], [7][1]]  # 가운데쪽 칸
+        # self.turn = '1'  # 플레이어의 턴 저장 변수 '1' 이면 1player, '2'이면 2player
+        # self.yutcount = 0 #누적 윷 갯수
+        # self.canthow = 0 # 윷 던질 수 있는 횟수
+
+        # 윷 확률 : 개(30%) 걸(26%) 도(12%) 빽도(12%) 낙(10%) 윷(5%) 모(5%)
+        for i in range(100):
+            if i < 30:
+                self.yutrandoms.append(2)  # 개
+            elif i < 56:
+                self.yutrandoms.append(3)  # 걸
+            elif i < 68:
+                self.yutrandoms.append(1)  # 도
+            elif i < 80:
+                self.yutrandoms.append(-1)  # 빽도
+            elif i < 90:
+                self.yutrandoms.append(0)  # 낙
+            elif i < 95:
+                self.yutrandoms.append(4)  # 윷
+            else:
+                self.yutrandoms.append(5)  # 모
+
+    # 이동할 칸 수를 리턴하는 메소드
+    def throw(self):
+
+        # 난수로 yuts의 자리값 선택
+        resultnum = random.randint(0, 99)
+        self.resultyut = ''
+
+        if self.yutrandoms[resultnum] == -1:
+            self.resultyut = "빽도"
+        elif self.yutrandoms[resultnum] ==0:
+            self.resultyut =  '낙'
+        elif self.yutrandoms[resultnum] == 1:
+            self.resultyut = '도'
+        elif self.yutrandoms[resultnum] == 2:
+            self.resultyut = '개'
+        elif self.yutrandoms[resultnum] == 3:
+            self.resultyut = '걸'
+        elif self.yutrandoms[resultnum] == 4:
+            self.resultyut = '윷'
+        elif self.yutrandoms[resultnum] == 5:
+            self.resultyut = '모'
+
+        # 이동 칸 수 반환
+        # return self.yutrandoms[resultnum]
+
+
+    ## 아래 함수들은 아직 메인함수랑 연동했을 때 오류가 떠서 주석처리 해놨습니다! ##
+
+    #턴 옮기는 함수 구현 후 더 구현할 예정
+    # def location(self):
+    #     self.shortcut = bool('지름길') #지름길 판별
+    #     #1P 첫번째 지름길
+    #     if self.player1_1 == self.yutArray[0][4]:
+    #         self.shortcut = '지름길'
+    #     elif self.player1_2 == self.yutArray[0][4]:
+    #         self.shortcut = '지름길'
+    #     elif self.player1_3 == self.yutArray[0][4]:
+    #         self.shortcut = '지름길'
+    #     elif self.player1_4 == self.yutArray[0][4]:
+    #         self.shortcut = '지름길'
+    #     #1P 두번째 지름길
+    #     elif self.player1_1 == self.yutArray[1][4]:
+    #         self.shortcut = '지름길'
+    #     elif self.player1_2 == self.yutArray[1][4]:
+    #         self.shortcut = '지름길'
+    #     elif self.player1_3 == self.yutArray[1][4]:
+    #         self.shortcut = '지름길'
+    #     elif self.player1_4 == self.yutArray[1][4]:
+    #         self.shortcut = '지름길'
+    #     #2P 첫번째 지름길
+    #     elif self.player2_1 == self.yutArray[0][4]:
+    #         self.shortcut = '지름길'
+    #     elif self.player2_2 == self.yutArray[0][4]:
+    #         self.shortcut = '지름길'
+    #     elif self.player2_3 == self.yutArray[0][4]:
+    #         self.shortcut = '지름길'
+    #     elif self.player2_4 == self.yutArray[0][4]:
+    #         self.shortcut = '지름길'
+    #     #2P 두번째 지름길
+    #     elif self.player2_1 == self.yutArray[1][4]:
+    #         self.shortcut = '지름길'
+    #     elif self.player2_2 == self.yutArray[1][4]:
+    #         self.shortcut = '지름길'
+    #     elif self.player2_3 == self.yutArray[1][4]:
+    #         self.shortcut = '지름길'
+    #     elif self.player2_4 == self.yutArray[1][4]:
+    #         self.shortcut = '지름길'
+    #
+    #     return self.shortcut
+    #
+    # #골판정 함수
+    # def Goal(self):
+    #     self.goal_1p = 0
+    #     self.goal_2p = 0
+    #     if self.player1_1 == self.yutArray[3][4]:
+    #         self.goal_1p = self.goal_1p + 1
+    #     elif self.player1_2 == self.yutArray[3][4]:
+    #         self.goal_1p = self.goal_1p + 1
+    #     elif self.player1_3 == self.yutArray[3][4]:
+    #         self.goal_1p = self.goal_1p + 1
+    #     elif self.player1_4 == self.yutArray[3][4]:
+    #         self.goal_1p = self.goal_1p + 1
+    #
+    #     if self.player2_1 == self.yutArray[3][4]:
+    #         self.goal_2p = self.goal_2p + 1
+    #     elif self.player2_2 == self.yutArray[3][4]:
+    #         self.goal_2p = self.goal_2p + 1
+    #     elif self.player2_3 == self.yutArray[3][4]:
+    #         self.goal_2p = self.goal_2p + 1
+    #     elif self.player2_4 == self.yutArray[3][4]:
+    #         self.goal_2p = self.goal_2p + 1
+    #
+    #     return self.goal_1p, self.goal_2p
+    #
+    # #게임 종료
+    # def GameOver(self):
+    #     self.text #승리 메세지 변수
+    #     if self.goal_1p == 4:
+    #         self.text = "플레이어 1이 이겼습니다!"
+    #     elif self.goal_2p == 4:
+    #         self.text = "플레이어 2가 이겼습니다!"
+
+
+# player1 = Yut()
+# print(player1.throw())
+# print(player1.pYut)
